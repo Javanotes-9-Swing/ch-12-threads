75f3cd1ea49c30bb865e09b4371c94f1d255d140
Todd Nordquist <toddnordquist144@gmail.com>
2025-11-16 15:18:46 -0700

    Initial Commit

---
+++ src/main/java/NamedThread.java
@@ -0,0 +1,25 @@
+
+
+
+public class NamedThread extends Thread {
+   private String name;  // The name of this thread.
+   public NamedThread(String name) {  // Constructor gives name to thread.
+      this.name = name;
+   }
+   public void run() {  // The run method prints a message to standard output.
+      System.out.println("Greetings from thread '" + name + "'!");
+   }
+
+	public static void main(String[] args) {
+		NamedThread greetings = new NamedThread("Fred");
+		System.out.println("Thread " + Thread.currentThread() +  " has been started");
+		greetings.start();
+
+//		System.out.println("This computer has " + Runtime.getRuntime().availableProcessors() + " processors.");
+
+		System.out.println("Default thread priority " + Thread.NORM_PRIORITY);
+		System.out.println("Min thread priority " + Thread.MIN_PRIORITY);
+
+		System.out.println("Priority of current thread " + Thread.currentThread().getPriority());
+	}
+}
