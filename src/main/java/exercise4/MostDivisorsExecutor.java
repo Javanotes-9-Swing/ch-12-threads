7d06be3b6e5ddd490b5304ceaf8cfd0935ace43e
Todd Nordquist <toddnordquist@gmail.com>
2026-02-03 20:57:20 -0700

    Solution in progress
    - Need to understand why method in callable appears not to be used.

--- src/main/java/exercise4/MostDivisorsExecutor.java
+++ src/main/java/exercise4/MostDivisorsExecutor.java
@@ -1,6 +1,7 @@
 package exercise4;
 
-import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.concurrent.*;
 
 /**
  * <p>
@@ -11,28 +12,95 @@ import java.util.concurrent.Callable;
  */
 public class MostDivisorsExecutor {
 
+    static void main() {
+        int numberOfTasks = 10;
+        countDivisorsWithExecutor(numberOfTasks);
+    }
+
+    private static void countDivisorsWithExecutor(int numberOfTasks) {
+        int processors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(processors);
+
+        /* An ArrayList used to store the Futures that are created when the tasks
+         * are submitted to the ExecutorService. */
+        ArrayList<Future<MostDivisors>> results = new ArrayList<>();
+
+        MostDivisors aTask = new MostDivisors(23, 89);
+        Future<MostDivisors> aResult = executor.submit(aTask);
+        results.add(aResult); // Save the Future representing the (future) result.
+
+        int maxMax = 0;
+        int maxMaxNum =0;
+        for (Future<MostDivisors> res : results) {
+            try {
+                int maxDiv = res.get().maxDiv;
+                int theNum = res.get().theNumDivided;
+                if (maxDiv > maxMax) {
+                    maxMaxNum = theNum;
+                    maxMax = maxDiv;
+                }
+            } catch (Exception e) {
+                // Should not occur in this program.  An exception can
+                // be thrown if the task was cancelled, if an exception
+                // occurred while the task was computing, or if the
+                // thread that is waiting on get() is interrupted.
+                System.out.println("Error occurred while computing: " + e);
+            }
+        }
+        System.out.println(maxMax + " is the number of divisors.");
+        System.out.println(maxMaxNum + " is the number with most divisors.");
+
+        executor.shutdown();
+
+    }
+
     /**
      * An object belonging to this class will count divisors for each integer in a specified range of integers.  The range is from min to max, inclusive, where min and max are given as parameters to the constructor.  The counting is done in the call() method, which returns the number of primes that were found.
      */
     private static class MostDivisors implements Callable<MostDivisors> {
         int min, max;
 
+        int maxDiv = 0;
+        int theNumDivided = 0;
+
         public MostDivisors(int min, int max) {
             this.min = min;
             this.max = max;
         }
 
+        public MostDivisors(short maxDiv, short theNumDivided) {
+            this.maxDiv = maxDiv;
+            this.theNumDivided = theNumDivided;
+        }
+
         @Override
         public MostDivisors call() {
-            MostDivisors md = calculateDivisorsOfNum(min, max);
-            return md;
+            return calculateDivisorsOfNum(min, max);
         }
 
         /**
          * Count the divisors between min and max, inclusive.
          */
         private MostDivisors calculateDivisorsOfNum(int min, int max) {
-            return null;
+
+            // loop from min to max getting each int
+            short maxDivs = (short) Integer.MIN_VALUE;
+            for (int i = min; i <= max; i++) {
+                // find the num divisors of each int
+                short divCount = 0;
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        divCount++;
+                    }
+                }
+                if (divCount > maxDivs) {
+                    maxDivs = divCount;
+                }
+            }
+
+            // keep track of max divisors
+
+            return new MostDivisors(maxDivs, theNumDivided);
         }
     }
 }
