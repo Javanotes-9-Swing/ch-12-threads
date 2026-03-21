81395da329207aef9d9f5bf77add7aae41db58b7
Todd Nordquist <toddnordquist@gmail.com>
2026-03-20 21:07:20 -0600

    Added to PIApproxGUI in progress.

--- src/main/java/exercise6/PIApproxGUI.java
+++ src/main/java/exercise6/PIApproxGUI.java
@@ -1,6 +1,7 @@
 package exercise6;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,16 +19,51 @@ public class PIApproxGUI extends JPanel implements ActionListener {
 
     //---------------------------------------------------------------------
 
+    final static Color labelBG = new Color(240,225,200);  // For creating labels
+    final static Color labelFG = new Color(180,0,0);
+    final static Font labelFont = new Font("Monospaced", Font.PLAIN, 12);
+
+
     private JLabel actualPI;        // Static display of value of PI.
-    private JLabel currentApprox;   // Current estimate of value of PI.
-    private JLabel numTrials;       // The number of times positions have been chosen.
+    private JLabel piEstimateLabel;   // Current estimate of value of PI.
+    private JLabel countLabel;       // The number of times positions have been chosen.
 
     private JButton runTrials;      // Run trials at choosing positions in the square.
 
     private PIApproximator piApproximator;
 
+    public PIApproxGUI() {
+
+        PIApproximator approximator = new PIApproximator();
+
+        piEstimateLabel =   makeLabel(" Current Estimate:  0");
+
+         /* Use a GridLayout with 4 rows and 1 column, and add all the
+          components that have been created to the panel. */
+
+        setBackground(Color.BLUE);
+        setLayout(new GridLayout(4,1,2,2));
+        add(piEstimateLabel);
+
+
+    } // end constructor
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    /**
+     * A utility routine for creating the labels that are used
+     * for display.  This routine is called by the constructor.
+     * @param text The text to show on the label.
+     */
+    private JLabel makeLabel(String text) {
+        JLabel label = new JLabel(text);
+        label.setBackground(labelBG);
+        label.setForeground(labelFG);
+        label.setFont(labelFont);
+        label.setOpaque(true);
+        return label;
     }
 }
--- src/main/java/exercise6/PIApproximator.java
+++ src/main/java/exercise6/PIApproximator.java
@@ -4,19 +4,19 @@ public class PIApproximator {
 
     private static final int BATCH_SIZE = 1000;
 
-    void trialGenerator() {
+    double trialGenerator() {
         long trialCount = 0;
         long inCircleCount = 0;
         for (int i = 0; i < BATCH_SIZE; i++) {
-        double x = Math.random();
-        double y = Math.random();
-        trialCount++;
-        if (x*x + y*y < 1)
-            inCircleCount++;
+            double x = Math.random();
+            double y = Math.random();
+            trialCount++;
+            if (x * x + y * y < 1)
+                inCircleCount++;
+        }
+        return 4 * ((double) inCircleCount / trialCount);
     }
-    double estimateForPi = 4 * ((double)inCircleCount / trialCount);
 
 //    countLabel.setText(      " Number of Trials:   " + trialCount);
 //    piEstimateLabel.setText( " Current Estimate:   " + estimateForPi);
-}
-}
+    }
