77d712289b5031c9d7e727b848157ffe1e1d878f
Todd Nordquist <toddnordquist@gmail.com>
2026-03-28 21:21:20 -0600

    Pi displays once; need it to run continuously.

--- src/main/java/exercise6/PIApproxGUI.java
+++ src/main/java/exercise6/PIApproxGUI.java
@@ -9,15 +9,25 @@ public class PIApproxGUI extends JPanel implements Runnable, ActionListener {
 
     public static final double BATCH_SIZE = 1000000;
     double estimateForPI = 0;
+    long trialCount = 0;
+    long inCircleCount = 0;
     private Timer timer;
+
+    private int status;   // Controls the execution of the thread; value is one of the following constants.
+
+    private static final int GO = 0;       // a value for status, meaning thread is to run continuously
+    private static final int PAUSE = 1;    // a value for status, meaning thread should not run
+    private static final int STEP = 2;     // a value for status, meaning thread should run one step then pause
+    private static final int RESTART = 3;  // a value for status, meaning thread should start again from the beginning
+
 
     public static void main(String[] args) {
         JFrame window = new JFrame("PI Approximator");
         PIApproxGUI content = new PIApproxGUI();
         window.setContentPane(content);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setLocation(1000,400);
-        window.setSize(350,200);
+        window.setLocation(1000, 400);
+        window.setSize(350, 200);
         window.setVisible(true);
     }
 
@@ -32,7 +42,7 @@ public class PIApproxGUI extends JPanel implements Runnable, ActionListener {
     private JLabel piEstimateLabel;   // Current estimate of value of PI.
     private JLabel countLabel;       // The number of times positions have been chosen.
 
-    private JButton trialsButton;      // Run trials at choosing positions in the square.
+    private JButton runPauseButton;      // Run trials at choosing positions in the square.
 
     private PIApproximator piApproximator;
 
@@ -41,26 +51,26 @@ public class PIApproxGUI extends JPanel implements Runnable, ActionListener {
         PIApproximator approximator = new PIApproximator();
 
         actualPI = makeLabel(" Actual value of pi: 3.141592653589793");
-        piEstimateLabel =   makeLabel(" Current Estimate:  0");
+        piEstimateLabel = makeLabel(" Current Estimate:  0");
         countLabel = makeLabel(" Number of trials:  0");
 
-        trialsButton = new JButton("Run");
-        trialsButton.addActionListener(this);
+        runPauseButton = new JButton("Run");
+        runPauseButton.addActionListener(this);
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.add(trialsButton);
+        buttonPanel.add(runPauseButton);
 
-        // --- Timer Setup ---
-        int delay = 1000; // 1000 milliseconds = 1 second
-        // 'this' refers to the current class instance which implements ActionListener
-        timer = new Timer(delay, this);
-        timer.start();
+//        // --- Timer Setup ---
+//        int delay = 100; // 1000 milliseconds = 1 second
+//        // 'this' refers to the current class instance which implements ActionListener
+//        timer = new Timer(delay, this);
+//        timer.start();
 
          /* Use a GridLayout with 4 rows and 1 column, and add all the
           components that have been created to the panel. */
 
         setBackground(Color.BLUE);
-        setLayout(new GridLayout(4,1,2,2));
+        setLayout(new GridLayout(4, 1, 2, 2));
         add(actualPI);
         add(piEstimateLabel);
         add(countLabel);
@@ -69,20 +79,37 @@ public class PIApproxGUI extends JPanel implements Runnable, ActionListener {
 
         /* Add a blue border around the panel. */
 
-        setBorder( BorderFactory.createLineBorder(Color.BLUE, 2) );
+        setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
 
         new Thread(this).start();
 
     } // end constructor
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    synchronized public void actionPerformed(ActionEvent evt) {
 
-//        if (secondsElapsed >= 10) {
-//            timer.stop();
-//            timeLabel.setText("Time: " + secondsElapsed + " seconds. Stopped.");
-//        }
+        Object source = evt.getSource();  // Object that generated
+        //   the action event.
+
+        if (source == runPauseButton) {
+            if (status == GO) {  // Animation is running. Pause it.
+                status = PAUSE;
+                runPauseButton.setText("Run");
+            } else {  // Animation is paused.  Start it running.
+                status = GO;
+                runPauseButton.setText("Pause");
+                // --- Timer Setup ---
+                int delay = 100; // 1000 milliseconds = 1 second
+                // 'this' refers to the current class instance which implements ActionListener
+                timer = new Timer(delay, this);
+                timer.start(); //TODO: Do I need a timer? If so, where is it called?
+                               // TODO: Where is the timer set up, constructor or method?
+            }
+
+        }
         piEstimateLabel.setText(" Current Estimate:  " + estimateForPI);
+        countLabel.setText(" Number of trials: " + this.trialCount);
+        notify();
     }
 
 //x`
@@ -91,28 +118,53 @@ public class PIApproxGUI extends JPanel implements Runnable, ActionListener {
     public void run() {
         while (true) {
 //            Continuously produce million round pi area trials
-            System.out.println(trialGenerator());
-            trialsButton.repaint();
+            runPauseButton.setText("Run");
+            status = PAUSE;
+            checkStatus(); // Returns only when user has clicked "Run"
+            try {
+                trialGenerator();
+            } catch (IllegalStateException e) {
+
+            }
         }
     }
 
-    double trialGenerator() {
-        long trialCount = 0;
-        long inCircleCount = 0;
+    /**
+     * This method is called before starting the solution. If the status is PAUSE, it waits until
+     * the status changes. When this method returns, the value of status must be RUN.
+     * (Note that this method requires synchronization, since
+     * otherwise calling wait() would produce an IllegalMonitorStateException.
+     * However, in fact, it is only called from other synchronized methods,
+     * so it would not be necessary to declare this method synchronized.
+     * Any method that calls it already owns the synchronization lock.)
+     */
+    synchronized private void checkStatus() {
+        while (status == PAUSE) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    synchronized void trialGenerator() {
+//        long trialCount = 0;
+//        long inCircleCount = 0;
         for (int i = 0; i < BATCH_SIZE; i++) {
             double x = Math.random();
             double y = Math.random();
-            trialCount++;
+            this.trialCount++;
             if (x * x + y * y < 1)
                 inCircleCount++;
         }
-        estimateForPI = 4 * ((double) inCircleCount / trialCount);
-        return estimateForPI;
+        estimateForPI = 4 * ((double) inCircleCount / this.trialCount);
+        checkStatus();
     }
 
     /**
      * A utility routine for creating the labels that are used
      * for display.  This routine is called by the constructor.
+     *
      * @param text The text to show on the label.
      */
     private JLabel makeLabel(String text) {
--- src/main/java/exercise6/TowersOfHanoiGUI.java
+++ src/main/java/exercise6/TowersOfHanoiGUI.java
@@ -6,20 +6,20 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 
 /**
- *  This program shows an animation of the famous Towers of Hanoi problem, for a pile
- *  of ten disks.  Three control buttons allow the user to control the animation.
- *  A "Next" button allows the user to see just one move in the solution.  Clicking
- *  the "Run" button will let the animation run on its own; while it is running,
- *  "Run" changes to "Pause", and clicking the button will pause the animation.
- *  A "Start Again" button allows the user to restart the problem from the beginning.
- *  
- *  The program is an example of using the wait() and notify() methods.  The
- *  wait() method is used to pause the animation between moves.  When the user
- *  clicks "Next" or "Run", the notify() method is called to notify the thread to
- *  wake up and continue.  A "status" variable is used to communicate commands to
- *  the thread.
- *  
- *  A main() routine allows this class to be run as an application. 
+ * This program shows an animation of the famous Towers of Hanoi problem, for a pile
+ * of ten disks.  Three control buttons allow the user to control the animation.
+ * A "Next" button allows the user to see just one move in the solution.  Clicking
+ * the "Run" button will let the animation run on its own; while it is running,
+ * "Run" changes to "Pause", and clicking the button will pause the animation.
+ * A "Start Again" button allows the user to restart the problem from the beginning.
+ * <p>
+ * The program is an example of using the wait() and notify() methods.  The
+ * wait() method is used to pause the animation between moves.  When the user
+ * clicks "Next" or "Run", the notify() method is called to notify the thread to
+ * wake up and continue.  A "status" variable is used to communicate commands to
+ * the thread.
+ * <p>
+ * A main() routine allows this class to be run as an application.
  */
 public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener {
 
@@ -34,14 +34,14 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.pack();
         window.setResizable(false);
-        window.setLocation(300,200);
+        window.setLocation(300, 200);
         window.setVisible(true);
     }
 
-    private static Color BACKGROUND_COLOR = new Color(255,255,180); // 4 colors used in drawing.
-    private static Color BORDER_COLOR = new Color(100,0,0);
-    private static Color DISK_COLOR = new Color(0,0,180);
-    private static Color MOVE_DISK_COLOR = new Color(180,180,255);
+    private static Color BACKGROUND_COLOR = new Color(255, 255, 180); // 4 colors used in drawing.
+    private static Color BORDER_COLOR = new Color(100, 0, 0);
+    private static Color DISK_COLOR = new Color(0, 0, 180);
+    private static Color MOVE_DISK_COLOR = new Color(180, 180, 255);
 
     private BufferedImage OSC;   // The off-screen canvas.  Frames are drawn here, then copied to the screen.
 
@@ -89,30 +89,30 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
     private class Display extends JPanel {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            int x = (getWidth() - OSC.getWidth())/2;
-            int y = (getHeight() - OSC.getHeight())/2;
+            int x = (getWidth() - OSC.getWidth()) / 2;
+            int y = (getHeight() - OSC.getHeight()) / 2;
             g.drawImage(OSC, x, y, null);
         }
     }
 
 
     /**
-     *  Create the panel, containing a display panel and, beneath it,
-     *  a sub-panel containing the three control buttons.  This
-     *  constructor also creates the off-screen canvas, and creates
-     *  and starts the animation thread.
+     * Create the panel, containing a display panel and, beneath it,
+     * a sub-panel containing the three control buttons.  This
+     * constructor also creates the off-screen canvas, and creates
+     * and starts the animation thread.
      */
-    public TowersOfHanoiGUI () {
-        OSC = new BufferedImage(430,143,BufferedImage.TYPE_INT_RGB);
+    public TowersOfHanoiGUI() {
+        OSC = new BufferedImage(430, 143, BufferedImage.TYPE_INT_RGB);
         display = new Display();
-        display.setPreferredSize(new Dimension(430,143));
+        display.setPreferredSize(new Dimension(430, 143));
         display.setBorder(BorderFactory.createLineBorder(BORDER_COLOR, 2));
         display.setBackground(BACKGROUND_COLOR);
         setLayout(new BorderLayout());
         add(display, BorderLayout.CENTER);
         JPanel buttonBar = new JPanel();
         add(buttonBar, BorderLayout.SOUTH);
-        buttonBar.setLayout(new GridLayout(1,0));
+        buttonBar.setLayout(new GridLayout(1, 0));
         runPauseButton = new JButton("Run");
         runPauseButton.addActionListener(this);
         buttonBar.add(runPauseButton);
@@ -128,28 +128,25 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
 
 
     /**
-     *  Event-handling method for the control buttons.  Changes in the
-     *  value of the status variable will be seen by the animation thread,
-     *  which will respond appropriately.
+     * Event-handling method for the control buttons.  Changes in the
+     * value of the status variable will be seen by the animation thread,
+     * which will respond appropriately.
      */
     synchronized public void actionPerformed(ActionEvent evt) {
         Object source = evt.getSource();
-        if (source == runPauseButton) {  // Toggle between running and paused.
+        if (source == runPauseButton) {      // Toggle between running and paused.
             if (status == GO) {  // Animation is running.  Pause it.
                 status = PAUSE;
                 nextStepButton.setEnabled(true);
                 runPauseButton.setText("Run");
-            }
-            else {  // Animation is paused.  Start it running.
+            } else {  // Animation is paused.  Start it running.
                 status = GO;
                 nextStepButton.setEnabled(false);  // Disabled when animation is running
                 runPauseButton.setText("Pause");
             }
-        }
-        else if (source == nextStepButton) {  // Set status to make animation run one step.
+        } else if (source == nextStepButton) {  // Set status to make animation run one step.
             status = STEP;
-        }
-        else if (source == startOverButton) { // Set status to make animation restart.
+        } else if (source == startOverButton) { // Set status to make animation restart.
             status = RESTART;
         }
         notify();  // Wake up the thread so it can see the new status value!
@@ -157,17 +154,17 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
 
 
     /**
-     *  The run() method for the animation thread.  Runs in an infinite loop.
-     *  In the loop, the thread first sets up the initial state of the "towers"
-     *  and of the buttons.  This includes setting the status to PAUSED, and
-     *  calling checkStatus(), which will not return until the user clicks the
-     *  "Run" button or the "Next" Button.  Once this happens, it calls
-     *  the solve() method to run the recursive algorithm that solves the
-     *  Towers Of Hanoi problem.  During the solution, checkStatus() is
-     *  called after each move.  If the user clicks the "Start Again" button,
-     *  checkStatus() will throw an IllegalStateException, which will cause
-     *  the solve() method to be aborted.  The exception is caught to prevent
-     *  it from crashing the thread.  
+     * The run() method for the animation thread.  Runs in an infinite loop.
+     * In the loop, the thread first sets up the initial state of the "towers"
+     * and of the buttons.  This includes setting the status to PAUSED, and
+     * calling checkStatus(), which will not return until the user clicks the
+     * "Run" button or the "Next" Button.  Once this happens, it calls
+     * the solve() method to run the recursive algorithm that solves the
+     * Towers Of Hanoi problem.  During the solution, checkStatus() is
+     * called after each move.  If the user clicks the "Start Again" button,
+     * checkStatus() will throw an IllegalStateException, which will cause
+     * the solve() method to be aborted.  The exception is caught to prevent
+     * it from crashing the thread.
      */
     public void run() {
         while (true) {
@@ -179,34 +176,32 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
             checkStatus(); // Returns only when user has clicked "Run" or "Next"
             startOverButton.setEnabled(true);
             try {
-                solve(10,0,1,2);  // Move 10 disks from pile 0 to pile 1.
-            }
-            catch (IllegalStateException e) {
+                solve(10, 0, 1, 2);  // Move 10 disks from pile 0 to pile 1.
+            } catch (IllegalStateException e) {
                 // Exception was thrown because user clicked "Start Over".
-            }            
+            }
         }
     }
 
 
     /**
-     *  This method is called before starting the solution and after each
-     *  move of the solution.  If the status is PAUSE, it waits until
-     *  the status changes.  If the status is RESTART, it throws
-     *  an IllegalStateException that will abort the solution.
-     *  When this method returns, the value of status must be 
-     *  RUN or STEP.
-     *     (Note that this method requires synchronization, since
-     *  otherwise calling wait() would produce an IllegalMonitorStateException.
-     *  However, in fact, it is only called from other synchronized methods,
-     *  so it would not be necessary to declare this method synchronized.
-     *  Any method that calls it already owns the synchronization lock.)
+     * This method is called before starting the solution and after each
+     * move of the solution.  If the status is PAUSE, it waits until
+     * the status changes.  If the status is RESTART, it throws
+     * an IllegalStateException that will abort the solution.
+     * When this method returns, the value of status must be
+     * RUN or STEP.
+     * (Note that this method requires synchronization, since
+     * otherwise calling wait() would produce an IllegalMonitorStateException.
+     * However, in fact, it is only called from other synchronized methods,
+     * so it would not be necessary to declare this method synchronized.
+     * Any method that calls it already owns the synchronization lock.)
      */
     synchronized private void checkStatus() {
         while (status == PAUSE) {
             try {
                 wait();
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
             }
         }
         // At this point, status is RUN, STEP, or RESTART.
@@ -218,10 +213,10 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
 
     /**
      * Sets up the initial state of the Towers Of Hanoi puzzle, with
-     * all the disks on the first pile.  
+     * all the disks on the first pile.
      */
     synchronized private void setUpProblem() {
-        moveDisk= 0;
+        moveDisk = 0;
         tower = new int[3][10];
         for (int i = 0; i < 10; i++)
             tower[0][i] = 10 - i;
@@ -239,18 +234,19 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
     /**
      * Solves the TowersOfHanoi problem to move the specified
      * number of disks from one pile to another.
+     *
      * @param disks the number of disks to be moved
-     * @param from the number of the pile where the disks are now
-     * @param to the number of the pile to which the disks are to be moved.
+     * @param from  the number of the pile where the disks are now
+     * @param to    the number of the pile to which the disks are to be moved.
      * @param spare the number of the pile that can be used as a spare.
      */
     private void solve(int disks, int from, int to, int spare) {
         if (disks == 1)
-            moveOne(from,to);
+            moveOne(from, to);
         else {
-            solve(disks-1, from, spare, to);
-            moveOne(from,to);
-            solve(disks-1, spare, to, from);
+            solve(disks - 1, from, spare, to);
+            moveOne(from, to);
+            solve(disks - 1, spare, to, from);
         }
     }
 
@@ -267,18 +263,18 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
      * checkStatus() method is called.
      */
     synchronized private void moveOne(int fromStack, int toStack) {
-        moveDisk = tower[fromStack][towerHeight[fromStack]-1];
+        moveDisk = tower[fromStack][towerHeight[fromStack] - 1];
         moveTower = fromStack;
         delay(120);
         towerHeight[fromStack]--;
-        putDisk(MOVE_DISK_COLOR,moveDisk,moveTower);
+        putDisk(MOVE_DISK_COLOR, moveDisk, moveTower);
         delay(80);
-        putDisk(BACKGROUND_COLOR,moveDisk,moveTower);
+        putDisk(BACKGROUND_COLOR, moveDisk, moveTower);
         delay(80);
         moveTower = toStack;
-        putDisk(MOVE_DISK_COLOR,moveDisk,moveTower);
+        putDisk(MOVE_DISK_COLOR, moveDisk, moveTower);
         delay(80);
-        putDisk(DISK_COLOR,moveDisk,moveTower);
+        putDisk(DISK_COLOR, moveDisk, moveTower);
         tower[toStack][towerHeight[toStack]] = moveDisk;
         towerHeight[toStack]++;
         moveDisk = 0;
@@ -295,8 +291,7 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
     synchronized private void delay(int milliseconds) {
         try {
             wait(milliseconds);
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
         }
     }
 
@@ -306,14 +301,15 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
      * used only during the moveOne() method, to draw the disk
      * that is being moved.  Calls display.repaint() to redraw
      * display using the newly modified image.
+     *
      * @param color the color of the disk (use background color to erase).
-     * @param disk the number of the disk that is to be drawn, 1 to 10.
-     * @param t the number of the pile on top of which the disk is drawn.
+     * @param disk  the number of the disk that is to be drawn, 1 to 10.
+     * @param t     the number of the pile on top of which the disk is drawn.
      */
     private void putDisk(Color color, int disk, int t) {
         Graphics g = OSC.getGraphics();
         g.setColor(color);
-        g.fillRoundRect(75+140*t - 5*disk - 5, 116-12*towerHeight[t], 10*disk+10, 10, 10, 10);
+        g.fillRoundRect(75 + 140 * t - 5 * disk - 5, 116 - 12 * towerHeight[t], 10 * disk + 10, 10, 10, 10);
         g.dispose();
         display.repaint();
     }
@@ -324,31 +320,30 @@ public class TowersOfHanoiGUI extends JPanel implements Runnable, ActionListener
      * if any, which is drawn as part of the moveOne() method.
      */
     synchronized private void drawCurrentFrame(Graphics g) {
-            // Called to draw the current frame.  But it is not drawn during
-            // the animation of the solution.  During the animation, the
-            // moveDisk() method just modifies the existing picture.
+        // Called to draw the current frame.  But it is not drawn during
+        // the animation of the solution.  During the animation, the
+        // moveDisk() method just modifies the existing picture.
         g.setColor(BACKGROUND_COLOR);
-        g.fillRect(0,0,430,143);
+        g.fillRect(0, 0, 430, 143);
         g.setColor(BORDER_COLOR);
         if (tower == null)
             return;
-        g.fillRect(10,128,130,5);
-        g.fillRect(150,128,130,5);
-        g.fillRect(290,128,130,5);
+        g.fillRect(10, 128, 130, 5);
+        g.fillRect(150, 128, 130, 5);
+        g.fillRect(290, 128, 130, 5);
         g.setColor(DISK_COLOR);
         for (int t = 0; t < 3; t++) {
             for (int i = 0; i < towerHeight[t]; i++) {
                 int disk = tower[t][i];
-                g.fillRoundRect(75+140*t - 5*disk - 5, 116-12*i, 10*disk+10, 10, 10, 10);
+                g.fillRoundRect(75 + 140 * t - 5 * disk - 5, 116 - 12 * i, 10 * disk + 10, 10, 10, 10);
             }
         }
         if (moveDisk > 0) {
             g.setColor(MOVE_DISK_COLOR);
-            g.fillRoundRect(75+140*moveTower - 5*moveDisk - 5, 116-12*towerHeight[moveTower], 
-                    10*moveDisk+10, 10, 10, 10);
+            g.fillRoundRect(75 + 140 * moveTower - 5 * moveDisk - 5, 116 - 12 * towerHeight[moveTower],
+                    10 * moveDisk + 10, 10, 10, 10);
         }
     }
-
 
 
 } // end class TowersOfHanoiGUI
